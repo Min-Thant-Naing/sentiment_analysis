2cc2467c8d97deeab9be7a5eff084ab130575d81
Min-Thant-Naing <89142969+Min-Thant-Naing@users.noreply.github.com>
2025-07-17 19:38:11 +0800

    Update sentiment_analyzer.py

--- sentiment_analyzer/sentiment_analyzer.py
+++ sentiment_analyzer/sentiment_analyzer.py
@@ -1,11 +1,10 @@
-import os
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 API_URL = "https://api-inference.huggingface.co/models/distilbert/distilbert-base-uncased-finetuned-sst-2-english"
-API_TOKEN = os.getenv("HF_API_KEY")
+API_TOKEN = "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"
 
 if not API_TOKEN:
     print("❌ No API key found. Check your .env file.")
@@ -15,8 +14,8 @@ headers = {
     "Authorization": f"Bearer {API_TOKEN}"
 }
 
-def sentiment_analyzer(text_to_analyse):
-    payload = {"inputs": text_to_analyse, "options": {"wait_for_model": True}}
-    r = requests.post(API_URL, headers=headers, json=payload, timeout=5)
-    r.raise_for_status()                         # Raise an error for bad responses
-    return r.json()[0][0] 
+def sentiment_analyzer(text):
+    payload = {"inputs": text, "options": {"wait_for_model": True}}
+    resp = requests.post(API_URL, headers=headers, json=payload, timeout=20)
+    resp.raise_for_status()          # raises if 4xx/5xx
+    return resp.json()  
